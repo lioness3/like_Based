207b319e790a7ca5bd43620f4129cd455e315908
Student <example@example.com>
2020-02-18 14:37:06 -0800

    create initial files

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,15 @@ function Header() {
   return (
     <div className='header'>
       <Link to='/'>Home</Link> | <Link to='/new'>New</Link>
-
+      <div className='dropDwn'>
+        <button className='sortBtn'> Sort by </button>
+          <div className='dropOptions'>
+            <button> option1</button>
+            <button>2 </button>
+            <button> 3</button>
+            <button>4 </button>
+          </div>
+      </div>
     </div>
   );
 }
--- src/components/Post.js
+++ src/components/Post.js
@@ -6,14 +6,16 @@ import './Post.css';
 
 
 function Post(props) {
-const [counter, setCounter] = useState(0);
+const [likeCounter, setLikeCounter] = useState(0);
+const [dislikeCounter, setDislikeCounter] = useState(0);
+  console.log("from post", likeCounter);
   return (
     <div>
-      <div>
-        <h3>{props.author}</h3>
+      <div className='post'>
+        <h3 style={{color:'purple'}}>{props.author}</h3>
         <p>{props.content}</p>
-        <p><button onClick={() => setCounter(counter + 1)}   style={{background:'inherit',border:'none'}}> 👍</button> {counter}</p>
-        <p><button onClick={() => setCounter(counter + 1)}  style={{background:'inherit',border:'none'}}> 👎</button>{counter}</p>
+        <p><button onClick={() => setLikeCounter(likeCounter + 1)}   style={{background:'inherit',border:'none'}}> 👍</button><span style={{color:'yellow'}}> {likeCounter}</span></p>
+        <p><button onClick={() => setDislikeCounter(dislikeCounter + 1)}  style={{background:'inherit',border:'none'}}> 👎</button><span style={{color:'yellow'}}>{dislikeCounter}</span></p>
       </div>
     </div>
   );
