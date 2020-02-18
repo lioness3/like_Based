02710e1e81bb7f3fc20443dd783eaf1c8f845287
Student <example@example.com>
2020-02-18 12:01:41 -0800

    add hook with local countere

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from "prop-types";
+import './Post.css';
+
 
 
 
 function Post(props) {
+const [counter, setCounter] = useState(0);
   return (
     <div>
       <div>
         <h3>{props.author}</h3>
         <p>{props.content}</p>
+        <p><button onClick={() => setCounter(counter + 1)}   style={{background:'inherit',border:'none'}}> 👍</button> {counter}</p>
+        <p><button onClick={() => setCounter(counter + 1)}  style={{background:'inherit',border:'none'}}> 👎</button>{counter}</p>
       </div>
     </div>
   );
